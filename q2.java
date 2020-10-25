5c481a3ae397c7bcb83677a73491c7ba3e295a72
wildhunterishere <73440486+wildhunterishere@users.noreply.github.com>
2020-10-26 00:10:23 +0530

    Add files via upload

--- q2.java
+++ q2.java
@@ -1,19 +1,19 @@
-package assn2;
+//2. Write JAVA program, which reads 4 numbers a, b, c and p. Let f(x)=ax2+bx+c be a function. The program outputs the value of f(p). e.g. input 4 3 –1 2 output 4(2)2+3(2)1= 21. 
+
+package Assignment1;
 
 import java.util.Scanner;
-
 public class q2 {
-
-	public static void main(String[] args) {
-		Scanner input=new Scanner(System.in);
-		int n=input.nextInt();
-		int sum=0;
-		for(int i=1;i<=n;i++) {
-			int num=input.nextInt();
-			sum+=num;
-		}
-		System.out.println(sum);
-
-	}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c, p, x;
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+        p = input.nextInt();
+        x = p;
+        x = ((a * (x * x)) + (b * x) + c);
+        System.out.print("Answer " + x);
+    }
 
 }
